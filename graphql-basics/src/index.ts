dbdd576798d9f5d96c6a45e2fce8e5fd7fb0b502
robbridges <robbridges@users.noreply.github.com>
2021-08-10 21:46:01 -0700

    Adding typescript support and basic subscription

--- graphql-basics/src/index.ts
+++ graphql-basics/src/index.ts
@@ -1,11 +1,14 @@
-import  {GraphQLServer} from 'graphql-yoga';
+import  {GraphQLServer, PubSub} from 'graphql-yoga';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
 import User from './resolvers/User';
 import Post from './resolvers/Post';
 import Comment from './resolvers/Comment';
+import Subscription from './resolvers/Subscription';
 
 import db from './db';
+
+const pubsub = new PubSub();
 
 
 const server = new GraphQLServer({
@@ -13,12 +16,14 @@ const server = new GraphQLServer({
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment
   },
   context: {
-    db
+    db,
+    pubsub
   }
 });
 
